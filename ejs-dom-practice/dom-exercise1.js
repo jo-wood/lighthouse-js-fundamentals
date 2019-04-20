3353b720d9277e5026acb5404c3ca5d3057573a4
Joanna Wood <caitlinjoannawood@gmail.com>
2019-04-20 00:54:40 +0000

    completed challenge

--- ejs-dom-practice/dom-exercise1.js
+++ ejs-dom-practice/dom-exercise1.js
@@ -8,57 +8,63 @@ var MOUNTAINS = [
     {name: "Mont Blanc", height: 4808, place: "Italy/France"}
   ];
 
+var root = document.querySelector("#mountains");
+var table = root.appendChild(document.createElement("table"));
+var row = table.appendChild(document.createElement("tr"));
+
+//create 2 arrays to store keys for making heading titles and
+//create MOUNTAINS as an array of arrays for each object's (mountain's) values
+var mountainsArray = [];
+var headingsArray = [];
+
+MOUNTAINS.forEach(function(mountObj){
+  headingsArray.push(Object.keys(mountObj));
+  mountainsArray.push(Object.values(mountObj));
+});
 
 
-
-  var root = document.querySelector("#mountains");
-  var table = root.appendChild(document.createElement("table"));
-  var row = table.appendChild(document.createElement("tr"));
-
-  var rowOne = row;
-
+//run through headingsArray once to capture keys for heading titles
+for (var h = 0; h < headingsArray.length; h++){
+ if (h === 0) {
   var titleOne = document.createElement("th");
-    titleOne.innerHTML = "name";
-    rowOne.appendChild(titleOne);
+    titleOne.innerHTML = headingsArray[0][0];
+    row.appendChild(titleOne);
+
   var titleTwo = document.createElement("th");
-    titleTwo.innerHTML = "height"
-    rowOne.appendChild(titleTwo);
+    titleTwo.innerHTML = headingsArray[0][1];
+    row.appendChild(titleTwo);
+
   var titleThree = document.createElement("th");
-    titleThree.innerHTML = "place";
-    rowOne.appendChild(titleThree);
+    titleThree.innerHTML = headingsArray[0][2];
+    row.appendChild(titleThree);
+     break;
+    }
+  }
 
+//loop through each mountain and add the 3 values per array into a new data cell
+for (var i = 0; i < mountainsArray.length; i++) {
+var newRow = table.insertRow();
 
-  var mountainsArray = [];
+  var newDataName = document.createElement("td");
+    newRow.appendChild(newDataName);
+  var newName = mountainsArray[i][0];
+    newDataName.innerHTML = newName;
 
-  MOUNTAINS.forEach(function(mountObj){
-    mountainsArray.push(Object.values(mountObj));
-  });
+  var newDataHeight = document.createElement("td");
+    newRow.appendChild(newDataHeight);
+  var newHeight = mountainsArray[i][1];
+    newDataHeight.innerHTML = newHeight;
 
-  for (var i = 0; i < mountainsArray.length; i++) {
+//style numerical values right per challenge question
+      var att = document.createAttribute("style");
+        att.value = 'text-align: right;';
+        newDataHeight.setAttributeNode(att);
 
-    var newRow = table.insertRow();
-
-    var newDataName = document.createElement("td");
-      newRow.appendChild(newDataName);
-    var newName = mountainsArray[i][0];
-      newDataName.innerHTML = newName;
-
-    var newDataHeight = document.createElement("td");
-      newRow.appendChild(newDataHeight);
-    var newHeight = mountainsArray[i][1];
-      newDataHeight.innerHTML = newHeight;
-
-    var newDataPlace = document.createElement("td");
-      newRow.appendChild(newDataPlace);
-    var newPlace = mountainsArray[i][2];
-      newDataPlace.innerHTML = newPlace;
-
-
+  var newDataPlace = document.createElement("td");
+    newRow.appendChild(newDataPlace);
+  var newPlace = mountainsArray[i][2];
+    newDataPlace.innerHTML = newPlace;
   }
 
 
-
 console.log(mountainsArray);
-
-
-//     var amountOfRows = mountainsArray.length;
